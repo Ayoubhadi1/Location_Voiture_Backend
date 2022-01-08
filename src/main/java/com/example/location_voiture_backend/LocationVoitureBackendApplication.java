77b4d56a2fc1aa753aa7bd53c2dd5931ff12f390
ayoub hadi <ayoub.hadi.stage@worldaudit.ma>
2022-01-08 01:22:41 +0000

    first commit

---
+++ src/main/java/com/example/location_voiture_backend/LocationVoitureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.location_voiture_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationVoitureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationVoitureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/location_voiture_backend/LocationVoitureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.location_voiture_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationVoitureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
